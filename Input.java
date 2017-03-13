9626d12e4759a8e78d1c93bad4166d06c66033af
Gerardas <gerardas.butkevicius@gamil.com>
2017-03-13 17:06:58 +0200

    Deleted system out Iveskite varda

--- Input.java
+++ Input.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Input {
  public  String betkas(){
     
-     System.out.println("Iveskite Varda");
         Scanner reader = new Scanner(System.in);
         String name= reader.nextLine();
         reader.close();
